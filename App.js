e4cd3c7f508013904fad058f4370387cc95e29cb
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-10-12 01:05:59 -0400

    mate card UI

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, ImageBackground, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <ImageBackground
+          source={{
+            uri: 'https://frenchiejourney.com/wp-content/uploads/2020/09/Merle-French-Bulldog.jpg'
+          }}
+          style={styles.image}
+        >
+          <View style={styles.cardInner}>
+            <Text style={styles.name}>Brinks</Text>
+            <Text style={styles.bio}>Ready to mate!</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 }
@@ -13,8 +24,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  bio: {
+    fontSize: 18,
+    color: '#FFF',
+    lineHeight: 24,
+  }
 });
