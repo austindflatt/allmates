7861ee802685a2d222c7119fb35935f90b9c2cca
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-10-14 22:46:20 -0400

    icons and navigation done

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Pressable, SafeAreaView } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 
 export default function App() {
+  const [activeScreen, setActiveScreen] = useState('HOME');
+
+  const color = '#000';
+  const activeColor = '#68B9F9';
   return (
-    <View style={styles.container}>
-      <MatchesScreen />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto name="paw" size={30} color={activeScreen === 'HOME' ? activeColor : color} />
+          </Pressable>
+          <MaterialCommunityIcons name="star-four-points" size={30} color={'#000'} />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons name="ios-chatbubbles" size={30} color={activeScreen === 'CHAT' ? activeColor : color} />
+          </Pressable>
+          <FontAwesome name="user" size={30} color={'#000'} />
+        </View>
+        {activeScreen === 'HOME' && <HomeScreen />}
+        {activeScreen === 'CHAT' && <MatchesScreen />}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding : 10,
+  }
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import Card from '../components/MateCard';
 import mates from '../../assets/data/mates';
 import AnimatedStack from '../components/AnimatedStack';
-
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 export default function HomeScreen() {
 
@@ -23,6 +24,17 @@ export default function HomeScreen() {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={30} color="#FBD88B" />
+        </View>
+        <View style={styles.button}>
+          <Entypo name="cross" size={30} color="#F76C6B" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={30} color="#4FCC94" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -32,6 +44,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-	width: '100%'
+    width: '100%',
   },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding : 10,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50
+  }
 });
